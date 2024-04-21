8ee9748d4d900b32971862e0299bf69b6d57492a
Jacooob <jacobhyun@uri.edu>
2024-04-21 11:43:56 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -25,6 +25,41 @@
                 <input type="password" name="password" id="password" required>
             </div>
             <div class="form-group">
+                <label for="firstname">First Name:</label>
+                <input type="text" name="firstname" id="firstname" required>
+            </div>
+            <div class="form-group">
+                <label for="lastname">Last Name:</label>
+                <input type="text" name="lastname" id="lastname" required>
+            </div>
+            <div class="form-group">
+                <label for="dob">Date of Birth:</label>
+                <input type="date" name="dob" id="dob" required>
+            </div>
+            <div class="form-group">
+                <label for="gender">Gender:</label>
+                <select name="gender" id="gender" required>
+                    <option value="M">Male</option>
+                    <option value="F">Female</option>
+                    <option value="O">Other</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="state">State:</label>
+                <select name="state" id="state" required>
+                    <option value="MA">Massachusetts</option>
+                    <option value="RI">Rhode Island</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="license_number">License Number:</label>
+                <input type="text" name="license_number" id="license_number" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" name="email" id="email" required>
+            </div>
+            <div class="form-group">
                 <label for="street">Street:</label>
                 <input type="text" name="street" id="street" required>
             </div>
@@ -36,6 +71,8 @@
                 <label for="zip">Zip:</label>
                 <input type="text" name="zip" id="zip" required>
             </div>
+            
+            
 
             <button type="submit">Sign Up</button>
         </form>
