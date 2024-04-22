1947171f11e91e5d5a917480590ddb8e99b5a817
alizarda <134419033+alizarda@users.noreply.github.com>
2024-04-22 19:07:17 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -30,31 +30,31 @@
 						<!-- Panel -->
 						<section class="panel color2-alt">
 							<div class="inner columns aligned">
-								<div class="span-4-5">
-									<h3 class="major">Sign Up</h3>
+								<div class="span-6-25">
+									<h3 class="major">Hunter Sign Up</h3>
 									<form action="signup-process.php" method="POST">
 										<div class="fields">
-											<div class="field">
+											<div class="field quarter">
 												<label for="username">Username:</label>
 												<input type="text" name="username" id="username" required>
 											</div>
-											<div class="field">
+											<div class="field quarter">
 												<label for="password">Password:</label>
 												<input type="password" name="password" id="password" required>
 											</div>
-											<div class="field">
+											<div class="field quarter">
 												<label for="firstname">First Name:</label>
 												<input type="text" name="firstname" id="firstname" required>
 											</div>
-											<div class="field">
+											<div class="field quarter">
 												<label for="lastname">Last Name:</label>
 												<input type="text" name="lastname" id="lastname" required>
 											</div>
-											<div class="field">
+											<div class="field quarter">
 												<label for="dob">Date of Birth:</label>
-												<input type="date" name="dob" id="dob" required>
+												<input type="date" name="dob" id="dob" required style="color: black;">
 											</div>
-											<div class="field">
+											<div class="field quarter">
 												<label for="gender">Gender:</label>
 												<select name="gender" id="gender" required>
 													<option value="M">Male</option>
@@ -62,30 +62,30 @@
 													<option value="O">Other</option>
 												</select>
 											</div>
-											<div class="field">
+											<div class="field quarter">
 												<label for="state">State:</label>
 												<select name="state" id="state" required>
 													<option value="MA">Massachusetts</option>
 													<option value="RI">Rhode Island</option>
 												</select>
 											</div>
-											<div class="field">
+											<div class="field quarter">
 												<label for="license_number">License Number:</label>
 												<input type="text" name="license_number" id="license_number" required>
 											</div>
-											<div class="field">
+											<div class="field quarter">
 												<label for="email">Email:</label>
 												<input type="email" name="email" id="email" required>
 											</div>
-											<div class="field">
+											<div class="field quarter">
 												<label for="street">Street:</label>
 												<input type="text" name="street" id="street" required>
 											</div>
-											<div class="field">
+											<div class="field quarter">
 												<label for="city">City:</label>
 												<input type="text" name="city" id="city" required>
 											</div>
-											<div class="field">
+											<div class="field quarter">
 												<label for="zip">Zip:</label>
 												<input type="text" name="zip" id="zip" required>
 											</div>
@@ -97,6 +97,78 @@
 								</div>
 							</div>
 						</section>		
+
+						<!-- Panel -->
+						<section class="panel color1-alt">
+							<div class="inner columns aligned">
+								<div class="span-6-25">
+									<h3 class="major">Warden Sign Up</h3>
+									<form action="signup-process.php" method="POST">
+										<div class="fields">
+											<div class="field quarter">
+												<label for="username">Username:</label>
+												<input type="text" name="username" id="username" required>
+											</div>
+											<div class="field quarter">
+												<label for="password">Password:</label>
+												<input type="password" name="password" id="password" required>
+											</div>
+											<div class="field quarter">
+												<label for="firstname">First Name:</label>
+												<input type="text" name="firstname" id="firstname" required>
+											</div>
+											<div class="field quarter">
+												<label for="lastname">Last Name:</label>
+												<input type="text" name="lastname" id="lastname" required>
+											</div>
+											<div class="field quarter">
+												<label for="dob">Date of Birth:</label>
+												<input type="date" name="dob" id="dob" required style="color: black;">
+											</div>
+											<div class="field quarter">
+												<label for="gender">Gender:</label>
+												<select name="gender" id="gender" required>
+													<option value="M">Male</option>
+													<option value="F">Female</option>
+													<option value="O">Other</option>
+												</select>
+											</div>
+											<div class="field quarter">
+												<label for="state">State:</label>
+												<select name="state" id="state" required>
+													<option value="MA">Massachusetts</option>
+													<option value="RI">Rhode Island</option>
+												</select>
+											</div>
+											<div class="field quarter">
+												<label for="license_number">License Number:</label>
+												<input type="text" name="license_number" id="license_number" required>
+											</div>
+											<div class="field quarter">
+												<label for="email">Email:</label>
+												<input type="email" name="email" id="email" required>
+											</div>
+											<div class="field quarter">
+												<label for="street">Street:</label>
+												<input type="text" name="street" id="street" required>
+											</div>
+											<div class="field quarter">
+												<label for="city">City:</label>
+												<input type="text" name="city" id="city" required>
+											</div>
+											<div class="field quarter">
+												<label for="zip">Zip:</label>
+												<input type="text" name="zip" id="zip" required>
+											</div>
+										</div>
+										<ul class="actions">
+											<li><input type="submit" value="Sign Up" class="primary color1" /></li>
+										</ul>
+									</form>
+								</div>
+							</div>
+						</section>		
+
 
 		<!-- Scripts -->
 			<script src="assets/js/jquery.min.js"></script>
