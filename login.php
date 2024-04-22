5772659e155f92f909494f1edb52c7909da9754a
alizarda <134419033+alizarda@users.noreply.github.com>
2024-04-22 17:25:19 -0400

    Update login.php

--- login.php
+++ login.php
@@ -4,29 +4,66 @@
 ?>
 
 <html>
-<?php include 'includes/head.php';?>
-    <body>
-        <?php include 'includes/header.php';?>
-        <center>
-            <h2>Warden Login</h2>
-            <h2>Enter Warden Email and Password
-                <form action='' method='post'>
-                    <label for="email">EMAIL: </label>
-                    <input type ='text' id='email' name='email'>
-                    <label for="password">PASSWORD: </label>
-                    <input type ='text' id='password' name='password'>
-                    <input type='submit' value='Login' name='Login'>
-                </form>
-                <?php
-                    
-                    if (isset($_POST['Login'])) {
-                        $_SESSION['email'] = $_POST['email'];
-                        $_SESSION['password'] = $_POST['password'];
-                        echo $_SESSION['email'];
-                        header( 'Location: ./postlogin.php' );
-                    }
-                ?>
-        <center>						 
-    </body>
-</html>
+	<?php include 'includes/head.php';?>
+	<body class="is-preload">
 
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+				<!-- Wrapper -->
+					<div id="wrapper">
+
+						<!-- Panel (Sidebar) -->
+						<section class="panel color6">
+								<div class="span-1">
+									<ul class="contact-icons" style="margin-left: 20px; color: black;">
+										<li class="fa fa-home"></li><a href="index.php">Home</a><br>
+										<li class="fa fa-globe"></li><a href="about.php">About</a><br>
+										<li class="fa fa-cogs"></li><a href="lookup.php">Lookup</a><br>
+										<li class="fa fa-cogs"></li><a href="update.php">Update</a><br>
+										<li class="fa fa-globe"></li><a href="login.php">Login</a><br>
+										<li class="fa fa-globe"></li><a href="signup.php">Sign Up</a><br>
+										<li class="fa fa-globe"></li><a href="warden.php">Warden</a><br>
+									</ul>					
+								</div>
+						</section>
+						
+            <!-- Panel -->
+            <section class="panel color2-alt">
+                <div class="inner columns aligned">
+                    <div class="span-4-5">
+                        <h3 class="major">Log-In</h3>
+                        <form method="post" action="">
+                            <div class="fields">
+                                <div class="field">
+                                    <label for="email">Email</label>
+                                    <input type="email" name="email" id="email" value="" placeholder="jane@untitled.tld" />
+                                </div>
+                                <div class="field">
+                                    <label for="password">Password</label>
+                                    <input type="password" name="password" id="password" value="" />
+                                </div>
+                            </div>
+                            <ul class="actions">
+                                <li><input type="submit" value="Log In" name="Login" class="primary color2" /></li>
+                            </ul>
+                        </form>
+                        <?php
+                            if (isset($_POST['Login'])) {
+                                $_SESSION['email'] = $_POST['email'];
+                                $_SESSION['password'] = $_POST['password'];
+                                echo $_SESSION['email'];
+                                header( 'Location: ./postlogin.php' );
+                            }
+                        ?>
+                    </div>
+                </div>
+            </section>
+			
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
