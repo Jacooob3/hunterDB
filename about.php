db7d573ae30ab309248d827671efcfef573801fc
alizarda <134419033+alizarda@users.noreply.github.com>
2024-04-22 16:42:01 -0400

    Update about.php

--- about.php
+++ about.php
@@ -27,15 +27,15 @@
 						<section class="panel color2-alt">
 								<div class="inner columns aligned">
 									<div class="span-3">
-										<h3 class="major">Paragraph 1</h3>
-										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do maecenas pharetra convallis posuere morbi leo urna molestie. Facilisi morbi tempus iaculis urna id volutpat. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Ultrices eros in cursus turpis massa.</p>
-										<h3 class="major">Paragraph 2</h3>
-										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do maecenas pharetra convallis posuere morbi leo urna molestie. Facilisi morbi tempus iaculis urna id volutpat. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Ultrices eros in cursus turpis massa.</p>
+										<h3 class="major">Introducing HuntDB</h3>
+										<p>The modern day hunting companion designed with the avid outdoorsman in mind, huntDB is a comprehensive toolkit for maximizing your hunting experience while ensuring compliance with local regulations. Whether you're a seasoned hunter or just starting out, huntDB equips you with everything you need to make your next hunt great.</p>
+										<h3 class="major">Know Your Hunt</h3>
+										<p>Use huntDB to track local hunting zones, ensuring you always know where you can legally pursue your quarry. Say goodbye to uncertainty and wasted time scouting for suitable locations – huntDB puts all the information you need right at your fingertips, empowering you to make informed decisions about where to hunt.</p>
 									</div>
 									<div class="span-3">
-										<h3 class="major">Paragraph 3</h3>
-										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do maecenas pharetra convallis posuere morbi leo urna molestie. Facilisi morbi tempus iaculis urna id volutpat. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Ultrices eros in cursus turpis massa.</p>
-										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do maecenas pharetra convallis posuere morbi leo urna molestie. Facilisi morbi tempus iaculis urna id volutpat. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Ultrices eros in cursus turpis massa.</p>
+										<h3 class="major">More Than a Tool</h3>
+										<p>An app designed with only the best in mind, huntDB gives you the confidence to focus on what you love most: the thrill of the chase. With features like comparing state laws, you can easily stay up-to-date with the latest regulations no matter where your hunting adventures take you. Use it anywhere for everywhere.</p>
+										<p>HuntDB is a comprehensive database that covers every aspect of the hunting experience, including what comes after. From registering your quarry to checking weapon legality, huntDB provides all the information you need to ensure a safe and legal hunt for yourself and others. No more guessing games or last-minute surprises.</p>
 									</div>
 
 								</section>
