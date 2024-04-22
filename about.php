fbb6d0b0b6a3ff02253ea22e36b951b736461892
alizarda <134419033+alizarda@users.noreply.github.com>
2024-04-22 10:50:06 -0400

    Update about.php

--- about.php
+++ about.php
@@ -1,17 +1,64 @@
+<?php require 'includes/database-connection.php';?> 
+
 <!DOCTYPE>
 <html>
 	<?php include 'includes/head.php';?>
-    <body>
-        <?php include 'includes/header.php';?>
- 
-		<main>
-			<div class="about-container">
-				<h1>HUNTER DATABASE</h1>
-				<hr />
-				<p>Paragraph 1</p>
-				<p>Paragraph 2</p>
-				<p>Paragraph 3</p>
+	<body class="is-preload">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+				<!-- Wrapper -->
+					<div id="wrapper">
+
+						<!-- Panel (Sidebar) -->
+						<section class="panel color2">
+								<div class="span-1">
+									<ul class="contact-icons color2" style="margin-left: 20px;">
+										<li class="fa fa-home"></li><a href="index.php">Home</a><br>
+										<li class="fa fa-globe"></li><a href="about.php">About</a><br>
+										<li class="fa fa-cogs"></li><a href="lookup.php">Lookup</a><br>
+										<li class="fa fa-globe"></li><a href="login.php">Account</a><br>
+									</ul>						
+								</div>
+							</section>
+							
+						<!-- Panel -->
+						<section class="panel color2-alt">
+								<div class="inner columns aligned">
+									<div class="span-3">
+										<h3 class="major">Paragraph 1</h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do maecenas pharetra convallis posuere morbi leo urna molestie. Facilisi morbi tempus iaculis urna id volutpat. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Ultrices eros in cursus turpis massa.</p>
+										<h3 class="major">Paragraph 2</h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do maecenas pharetra convallis posuere morbi leo urna molestie. Facilisi morbi tempus iaculis urna id volutpat. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Ultrices eros in cursus turpis massa.</p>
+									</div>
+									<div class="span-3">
+										<h3 class="major">Paragraph 3</h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do maecenas pharetra convallis posuere morbi leo urna molestie. Facilisi morbi tempus iaculis urna id volutpat. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Ultrices eros in cursus turpis massa.</p>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do maecenas pharetra convallis posuere morbi leo urna molestie. Facilisi morbi tempus iaculis urna id volutpat. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Ultrices eros in cursus turpis massa.</p>
+									</div>
+
+								</section>
+									<!-- Panel -->
+									<section class="panel">
+											<div class="gallery">
+											<a href="images/gallery/fulls/04.jpg" class="image filtered span-2-5" data-position="top"><img src="images/gallery/thumbs/04.jpg" alt="" /></a>
+												<div class="group span-3">
+													<a href="images/gallery/fulls/01.jpg" class="image filtered span-3" data-position="bottom"><img src="images/gallery/thumbs/01.jpg" alt="" /></a>
+													<a href="images/gallery/fulls/02.jpg" class="image filtered span-1-5" data-position="center"><img src="images/gallery/thumbs/02.jpg" alt="" /></a>
+													<a href="images/gallery/fulls/03.jpg" class="image filtered span-1-5" data-position="bottom"><img src="images/gallery/thumbs/03.jpg" alt="" /></a>
+												</div>
+												<a href="images/gallery/fulls/04.jpg" class="image filtered span-2-5" data-position="top"><img src="images/gallery/thumbs/04.jpg" alt="" /></a>
+											</div>
+										</section>
+
+
 			</div>
-		</main>
+			
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/main.js"></script>
+
 	</body>
 </html>
