fadfa66022712939e772af1912c3c7cf2277084f
alizarda <134419033+alizarda@users.noreply.github.com>
2024-04-22 17:05:16 -0400

    Update lookup.php

--- lookup.php
+++ lookup.php
@@ -14,24 +14,29 @@
 						<section class="panel color6">
 								<div class="span-1">
 									<ul class="contact-icons" style="margin-left: 20px; color: black;">
-										<li class="fa fa-home"></li><a href="index.php" style="color: black;">Home</a><br>
-										<li class="fa fa-globe"></li><a href="about.php" style="color: black;">About</a><br>
-										<li class="fa fa-cogs"></li><a href="lookup.php" style="color: black;">Lookup</a><br>
-										<li class="fa fa-cogs"></li><a href="update.php" style="color: black;">Update</a><br>
-										<li class="fa fa-globe"></li><a href="account.php" style="color: black;">Account</a><br>
+										<li class="fa fa-home"></li><a href="index.php">Home</a><br>
+										<li class="fa fa-globe"></li><a href="about.php">About</a><br>
+										<li class="fa fa-cogs"></li><a href="lookup.php">Lookup</a><br>
+										<li class="fa fa-cogs"></li><a href="update.php">Update</a><br>
+										<li class="fa fa-globe"></li><a href="login.php">Login</a><br>
+										<li class="fa fa-globe"></li><a href="signup.php">Sign Up</a><br>
+										<li class="fa fa-globe"></li><a href="warden.php">Warden</a><br>
+									</ul>					
+								</div>
+						</section>
+						
+						<!-- Panel (Sidebar) -->
+						<section class="panel color3">
+								<div class="span-1">
+									<ul class="contact-icons" style="margin-left: 20px; color: black;">
+										<li class="fa fa-home"></li><a href="lookupanimalpop.php">Look Up Animal Population</a><br>
+										<li class="fa fa-globe"></li><a href="lookuphunter.php">Look Up Hunter</a><br>
 									</ul>					
 								</div>
 						</section>
 
 						<!-- Panel -->
 						<section class="panel color2-alt">
-								<div class="intro color2">
-									<h2 class="major">asdsadas</h2>
-                                    <a href="listanimalpop.php" class="button">Animal Pop</a>
-                                    <a href="displayhunter.php" class="button">Display Hunter</a>
-									<a href="lookupanimalpop.php" class="button">Look Up Animal Pop</a>
-                                    <a href="lookuphunter.php" class="button">Look Up Hunter</a>
-								</div>
 								<div class="inner columns aligned">
 									<div class="span-4-5">
 										<h3 class="major">Form</h3>
