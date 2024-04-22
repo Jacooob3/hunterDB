f02ce4e14ff60946a6598f5260f25c2ce3a3f8ee
alizarda <134419033+alizarda@users.noreply.github.com>
2024-04-22 18:42:17 -0400

    Update login.php

--- login.php
+++ login.php
@@ -12,26 +12,58 @@
 				<!-- Wrapper -->
 					<div id="wrapper">
 
-						<!-- Panel (Sidebar) -->
-						<section class="panel color6">
-								<div class="span-1">
-									<ul class="contact-icons" style="margin-left: 20px; color: black;">
-										<li class="fa fa-home"></li><a href="index.php">Home</a><br>
-										<li class="fa fa-globe"></li><a href="about.php">About</a><br>
-										<li class="fa fa-cogs"></li><a href="lookup.php">Lookup</a><br>
-										<li class="fa fa-cogs"></li><a href="update.php">Update</a><br>
-										<li class="fa fa-globe"></li><a href="login.php">Login</a><br>
-										<li class="fa fa-globe"></li><a href="signup.php">Sign Up</a><br>
-										<li class="fa fa-globe"></li><a href="warden.php">Warden</a><br>
-									</ul>					
-								</div>
-						</section>
+                <!-- Panel (Sidebar) -->
+                <section class="panel color6">
+                        <div class="span-1">
+                            <ul class="contact-icons" style="margin-left: 20px; color: black;">
+                                <li class="fa fa-home"></li><a href="index.php">Home</a><br>
+                                <li class="fa fa-globe"></li><a href="about.php">About</a><br>
+                                <li class="fa fa-cogs"></li><a href="lookup.php">Lookup</a><br>
+                                <li class="fa fa-cogs"></li><a href="update.php">Update</a><br>
+                                <li class="fa fa-globe"></li><a href="login.php">Login</a><br>
+                                <li class="fa fa-globe"></li><a href="signup.php">Sign Up</a><br>
+                                <li class="fa fa-globe"></li><a href="warden.php">Warden</a><br>
+                            </ul>					
+                        </div>
+                </section>
 						
-            <!-- Panel -->
-            <section class="panel color2-alt">
+                <!-- Panel -->
+                <section class="panel color2-alt">
+                    <div class="inner columns aligned">
+                        <div class="span-4-5">
+                            <h3 class="major">Hunter Log-In</h3>
+                            <form method="post" action="">
+                                <div class="fields">
+                                    <div class="field">
+                                        <label for="email">Email</label>
+                                        <input type="email" name="email" id="email" value="" placeholder="john@untitled.tld" />
+                                    </div>
+                                    <div class="field">
+                                        <label for="password">Password</label>
+                                        <input type="password" name="password" id="password" value="" />
+                                    </div>
+                                </div>
+                                <ul class="actions">
+                                    <li><input type="submit" value="Log In" name="Login" class="primary color2" /></li>
+                                </ul>
+                            </form>
+                            <?php
+                                if (isset($_POST['Login'])) {
+                                    $_SESSION['email'] = $_POST['email'];
+                                    $_SESSION['password'] = $_POST['password'];
+                                    echo $_SESSION['email'];
+                                    header( 'Location: ./postlogin.php' );
+                                }
+                            ?>
+                        </div>
+                    </div>
+                </section>
+
+                <!-- Panel -->
+                <section class="panel color1-alt">
                 <div class="inner columns aligned">
                     <div class="span-4-5">
-                        <h3 class="major">Log-In</h3>
+                        <h3 class="major">Warden Log-In</h3>
                         <form method="post" action="">
                             <div class="fields">
                                 <div class="field">
@@ -58,7 +90,7 @@
                     </div>
                 </div>
             </section>
-			
+
 		<!-- Scripts -->
 			<script src="assets/js/jquery.min.js"></script>
 			<script src="assets/js/browser.min.js"></script>
