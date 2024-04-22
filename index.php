2cbc6aeb36e940e7a0376b50a7d9c5dcfde8f1de
alizarda <134419033+alizarda@users.noreply.github.com>
2024-04-22 10:57:17 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<?php   										// Opening PHP tag
-	// Include the database connection script
+<?php
 	require 'includes/database-connection.php';
 	
 	// Fetch recent hunting activities
@@ -10,52 +9,106 @@
 		echo "Error: " . $e->getMessage();
 	}
 
-?> 
+?>
 
 <!DOCTYPE>
+
 <html>
-	<head>
-		<title>Hunting Database</title>
-	</head>
 	<?php include 'includes/head.php';?>
-    <body>
-        <?php include 'includes/header.php';?>
-		<main>
-			<section>
+	<body class="is-preload">
 
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+				<!-- Wrapper -->
+					<div id="wrapper">
 
-				<div class="welcome">
-					<h2>Welcome to the Hunting Database!</h2>
-                    <br>
+						<!-- Panel (Sidebar) -->
+						<section class="panel color2">
+								<div class="span-1">
+									<ul class="contact-icons color2" style="margin-left: 20px;">
+										<li class="fa fa-home"></li><a href="index.php">Home</a><br>
+										<li class="fa fa-globe"></li><a href="about.php">About</a><br>
+										<li class="fa fa-cogs"></li><a href="lookup.php">Lookup</a><br>
+										<li class="fa fa-globe"></li><a href="login.php">Account</a><br>
+									</ul>						
+								</div>
+							</section>
+							
+						<!-- Panel (Landing) -->
+							<section class="panel banner right">
+								<div class="content color0 span-3-75">
+									<h1 class="major">huntDB</h1>
+									<p>Welcome to <strong>huntDB</strong>, a free, comprehensive resource to catalog hunting data. Dive into the  insights and resources we have available for you to curate your next great hunt.</p>
+									<div class="buttons">
+										<a href="lookuphunter.php" class="button">Look Up Hunter   </a>
+										<a href="lookupanimalpop.php" class="button">Look Up Animal Population   </a>
+									</div>
+								</div>
+								<div class="image filtered tinted span-1-75" data-position="25% 25%">
+									<img src="images/huntTall.png" alt="" />
+								</div>
+							</section>
 
-					<p>Please login or sign up to access the database.</p>
-					<div class="buttons">
-						<a href="lookuphunter.php" class="button">Look Up Hunter   </a>
-						<a href="lookupanimalpop.php" class="button">Look Up Animal Population   </a>
-						
-					</div>
+						<!-- Panel (Spotlight) -->
+						<section class="panel spotlight medium left">
+								<div class="content span-7">
+									<h2 class="major">Hunt Your Way</h2>
+									<p>Take advantage of your in-site tools to plan your hunt--whether that be checking your weapon, your environment, or your bounty.</p>
+								</div>
+								<div class="image filtered tinted" data-position="top left">
+									<img src="images/huntLook.png" alt="" />
+								</div>
+						</section>
+
+						<!-- Panel (Navigation) -->
+						<section class="panel spotlight medium left">
+								<div class="intro joined">
+									<h2 class="major">Database Resource</h2>
+									<p>Cross reference our catalog of database information to ensure you abide by all state laws.</p>
+								</div>
+								<div class="inner">
+									<ul class="grid-icons three connected" style="margin-left: 40px;">
+										<li><span class="icon solid fa-tree"><span class="label">State</span></span></li>
+										<li><span class="icon solid fa-paw"><span class="label">Animal</span></span></li>
+										<li><span class="icon solid fa-bullseye"><span class="label">Weapon</span></span></li>
+									</ul>
+								</div>
+								<div class="image filtered tinted" data-position="top left">
+									<img src="images/huntStare.png" alt="" />
+								</div>
+							</section>
+
 				</div>
-				<div class="content">
-					<!-- Your existing content -->
-					<aside class="recent-hunts">
-						<h3>Recent Hunts</h3>
-						<ul>
-							<?php foreach ($recentHunts as $hunt): ?>
-								<li>
-									<strong>Hunter:</strong> <?= htmlspecialchars($hunt['hunterName']) ?><br>
-									<strong>Animal:</strong> <?= htmlspecialchars($hunt['animalName']) ?><br>
-									<strong>Weapon:</strong> <?= htmlspecialchars($hunt['weaponName']) ?><br>
-									<strong>Info:</strong> <?= htmlspecialchars($hunt['additional_info']) ?>
-								</li>
-							<?php endforeach; ?>
-						</ul>
-					</aside>
-				</div>
+			</div>
+			
+						<!-- Panel (Spotlight) -->
+						<section class="panel spotlight medium left">
+								<div class="content span-7">
+									<aside class="recent-hunts">
+										<h3>Recent Hunts</h3>
+										<ul>
+											<?php foreach ($recentHunts as $hunt): ?>
+												<li>
+													<strong>Hunter:</strong> <?= htmlspecialchars($hunt['hunterName']) ?><br>
+													<strong>Animal:</strong> <?= htmlspecialchars($hunt['animalName']) ?><br>
+													<strong>Weapon:</strong> <?= htmlspecialchars($hunt['weaponName']) ?><br>
+													<strong>Info:</strong> <?= htmlspecialchars($hunt['additional_info']) ?>
+												</li>
+											<?php endforeach; ?>
+										</ul>
+									</aside>
+								</div>
+						</section>
 
-			</section>
-		</main>
+
+
+
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/main.js"></script>
+
 	</body>
 </html>
-
-
-
