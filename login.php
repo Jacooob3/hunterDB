04afd42b327d6d3c7f47b630b41fada64be9065f
Jacob Hyun <98486911+Jacooob3@users.noreply.github.com>
2024-04-23 19:55:45 -0400

    hunter login

--- login.php
+++ login.php
@@ -12,25 +12,49 @@
 				<!-- Wrapper -->
 					<div id="wrapper">
 
-		<!-- Panel (Sidebar) -->
-		<section class="panel color6">
-				<div class="span-1">
-					<ul class="contact-icons" style="margin-left: 20px; color: black;">
-						<li class="fa fa-home"></li><a href="index.php">Home</a><br>
-						<li class="fa fa-address-card"></li><a href="about.php">About</a><br>
-						<li class="fa fa-search"></li><a href="lookup.php">Lookup</a><br>
-						<li class="fa fa-user-lock"></li><a href="login.php">Login</a><br>
-						<li class="fa fa-user-plus"></li><a href="signup.php">Sign Up</a><br>
-					</ul>					
-				</div>
-		</section>
-		
-                <!-- Panel -->
-                <section class="panel color1-alt">
+            <!-- Panel (Sidebar) -->
+            <section class="panel color6">
+                    <div class="span-1">
+                        <ul class="contact-icons" style="margin-left: 20px; color: black;">
+                            <li class="fa fa-home"></li><a href="index.php">Home</a><br>
+                            <li class="fa fa-address-card"></li><a href="about.php">About</a><br>
+                            <li class="fa fa-search"></li><a href="lookup.php">Lookup</a><br>
+                            <li class="fa fa-user-lock"></li><a href="login.php">Login</a><br>
+                            <li class="fa fa-user-plus"></li><a href="signup.php">Sign Up</a><br>
+                        </ul>					
+                    </div>
+            </section>
+            
+            <!-- Panel -->
+            <section class="panel color1-alt">
                 <div class="inner columns aligned">
                     <div class="span-4-5">
                         <h3 class="major">Warden Log-In</h3>
                         <form action="postlogin.php" method="POST" >
+                            <div class="fields">
+                                <div class="field">
+                                    <label for="email">Email</label>
+                                    <input type="email" name="email" id="email" value="" placeholder="jane@untitled.tld" />
+                                </div>
+                                <div class="field">
+                                    <label for="password">Password</label>
+                                    <input type="password" name="password" id="password" value="" />
+                                </div>
+                            </div>
+                            <ul class="actions">
+                                <li><input type="submit" value="Log In" name="Login" class="primary color2" /></li>
+                            </ul>
+                        </form>
+                        
+                    </div>
+                </div>
+            </section>
+            <!-- Panel -->
+            <section class="panel color1-alt">
+                <div class="inner columns aligned">
+                    <div class="span-4-5">
+                        <h3 class="major">Hunter Log-In</h3>
+                        <form action="postlogin-hunter.php" method="POST" >
                             <div class="fields">
                                 <div class="field">
                                     <label for="email">Email</label>
---
+++ postlogin-hunter.php
@@ -0,0 +1,77 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+session_start();
+require 'includes/database-connection.php';
+
+$email = $_POST['email'];
+$pass = $_POST['password'];
+
+// Prepare a statement for execution
+$stmt = $pdo->prepare("SELECT hunter_id, password FROM hunter WHERE email = :email");
+$stmt->bindParam(':email', $email);
+$stmt->execute();
+
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($user && password_verify($pass, $user['pass'])) {
+    // Successful login
+    $_SESSION['logged_in'] = true;
+    $_SESSION['role'] = 'hunter';
+    $_SESSION['id'] = $user['hunter_id'];
+    $redirectUrl = 'account.php';
+    $message = "Login successful. Redirecting to update page...";
+} else {
+    // Invalid credentials
+    $_SESSION['error'] = 'Invalid username or password';
+    $redirectUrl = 'login.php';
+    $message = "Login unsuccessful. Redirecting back to login page...";
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include 'includes/head.php';?>
+    <meta http-equiv="refresh" content="3;url=<?= $redirectUrl ?>">
+</head>
+<body class="is-preload">
+    <div id="page-wrapper">
+        <div id="wrapper">
+            <section class="panel color6">
+                <div class="span-1">
+                    <ul class="contact-icons" style="margin-left: 20px; color: black;">
+                        <li class="fa fa-home"></li><a href="index.php">Home</a><br>
+                        <li class="fa fa-address-card"></li><a href="about.php">About</a><br>
+                        <li class="fa fa-search"></li><a href="lookup.php">Lookup</a><br>
+                        <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']): ?>
+                            <?php if ($_SESSION['role'] == 'warden'): ?>
+                                <li class="fa fa-tag"></li><a href="update.php">Warden</a><br>
+                            <?php endif; ?>
+                            <li class="fa fa-user"></li><a href="account.php">Account</a><br>
+                            <li class="fa fa-user-lock"></li><a href="logout.php">Logout</a><br>
+                        <?php else: ?>
+                            <li class="fa fa-user-lock"></li><a href="login.php">Login</a><br>
+                            <li class="fa fa-user-plus"></li><a href="signup.php">Sign Up</a><br>
+                        <?php endif; ?>
+                    </ul>                 
+                </div>
+            </section>
+
+            <section class="panel color1-alt">
+                <div class="inner columns aligned">
+                    <div class="span-4-5">
+                        <h2><?= $message ?></h2>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/browser.min.js"></script>
+    <script src="assets/js/breakpoints.min.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+</html>
