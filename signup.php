acb4d64a80d8233e476fc844b379aad26b38f37d
Jacooob <jacobhyun@uri.edu>
2024-04-23 15:06:29 -0400

    fix

--- signup.php
+++ signup.php
@@ -18,6 +18,7 @@
 									<ul class="contact-icons" style="margin-left: 20px; color: black;">
 										<li class="fa fa-home"></li><a href="index.php">Home</a><br>
 										<li class="fa fa-address-card"></li><a href="about.php">About</a><br>
+										<li class="fa fa-search"></li><a href="lookup.php">Lookup</a><br>
 										<li class="fa fa-user-lock"></li><a href="login.php">Login</a><br>
 										<li class="fa fa-user-plus"></li><a href="signup.php">Sign Up</a><br>
 									</ul>					
