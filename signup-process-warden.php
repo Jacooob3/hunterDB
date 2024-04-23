1dbe0ba64cb4bb2d0bdeb3aea077e8ddaf1746da
Jacob Hyun <98486911+Jacooob3@users.noreply.github.com>
2024-04-23 19:44:34 -0400

    Update signup-process-warden.php

--- signup-process-warden.php
+++ signup-process-warden.php
@@ -7,6 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $lastname = trim($_POST['lastname']);
     $state = trim($_POST['state']); 
     $email = trim($_POST['email']);
+    $phone = trim($_POST['phonenum']);
     $password = trim($_POST['password']);
 
     // Validate input
@@ -26,19 +27,68 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $pdo->beginTransaction();
 
         
-
-        $stmt = $pdo->prepare("INSERT INTO game_warden (warden_id, state_id, fname, lname, email, pass) VALUES (?, ? , ?, ?, ?, ?)");
-        $stmt->execute([$warden_id, $state, $firstname, $lastname,  $email, $hashed_password]);
+        $stmt = $pdo->prepare("INSERT INTO game_warden (warden_id, state_id, fname, lname, email, phonenum, pass) VALUES (?, ? , ?, ?, ?, ?, ?)");
+        $stmt->execute([$warden_id, $state, $firstname, $lastname,  $email, $phone, $hashed_password]);
 
         $pdo->commit();
-
-        echo "Signup successful. <a href='login.php'>Login here</a>";
+        // Successful login
+        $redirectUrl = 'login.php';
+        $message = "Signup successful. Redirecting to update page...";
     } catch (PDOException $e) {
         $pdo->rollBack();
-        echo "Error: " . $e->getMessage();
-    }
+    // Invalid credentials
+    $_SESSION['error'] = 'Invalid username or password';
+    $redirectUrl = 'signup.php';
+    $message = "Signup unsuccessful. Redirecting back to login page...";    }
 } else {
     // Not a POST request
     echo "Invalid request.";
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include 'includes/head.php';?>
+    <meta http-equiv="refresh" content="3;url=<?= $redirectUrl ?>">
+</head>
+<body class="is-preload">
+    <div id="page-wrapper">
+        <div id="wrapper">
+            <section class="panel color6">
+                <div class="span-1">
+                    <ul class="contact-icons" style="margin-left: 20px; color: black;">
+                        <li class="fa fa-home"></li><a href="index.php">Home</a><br>
+                        <li class="fa fa-address-card"></li><a href="about.php">About</a><br>
+                        <li class="fa fa-search"></li><a href="lookup.php">Lookup</a><br>
+                        <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']): ?>
+                            <?php if ($_SESSION['role'] == 'warden'): ?>
+                                <li class="fa fa-tag"></li><a href="update.php">Warden</a><br>
+                            <?php endif; ?>
+                            <li class="fa fa-user"></li><a href="account.php">Account</a><br>
+                            <li class="fa fa-user-lock"></li><a href="logout.php">Logout</a><br>
+                        <?php else: ?>
+                            <li class="fa fa-user-lock"></li><a href="login.php">Login</a><br>
+                            <li class="fa fa-user-plus"></li><a href="signup.php">Sign Up</a><br>
+                        <?php endif; ?>
+                    </ul>                 
+                </div>
+            </section>
+
+            <section class="panel color1-alt">
+                <div class="inner columns aligned">
+                    <div class="span-4-5">
+                        <h2><?= $message ?></h2>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/browser.min.js"></script>
+    <script src="assets/js/breakpoints.min.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+</html>
