c23340ddd69a6bcdb2d23febecf28abb63fb2b17
alizarda <134419033+alizarda@users.noreply.github.com>
2024-04-22 20:53:52 -0400

    Update displayhunter.php

--- displayhunter.php
+++ displayhunter.php
@@ -51,15 +51,15 @@
                         </div>
                 </section>
 
-			<!-- Panel (Sidebar) -->
+				<!-- Panel (Sidebar) -->
 				<section class="panel color3">
-					<div class="span-1">
-						<ul class="contact-icons" style="margin-left: 20px; color: black;">
-							<li class="fa fa-home"></li><a href="lookupanimalpop.php">Look Up Animal Population</a><br>
-							<li class="fa fa-globe"></li><a href="lookuphunter.php">Look Up Hunter</a><br>
-						</ul>					
-					</div>
-			</section>
+						<div class="span-1">
+							<ul class="contact-icons" style="margin-left: 20px; color: black;">
+								<li class="fa fa-home"></li><a href="lookupanimalpop.php">Animal</a><br>
+								<li class="fa fa-globe"></li><a href="lookuphunter.php">Hunter</a><br>
+							</ul>					
+						</div>
+				</section>
 
 
 			<!-- Panel -->
