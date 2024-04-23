09c1e4a91cdf75df8587d9a4ed6d20069e5fc6e4
alizarda <134419033+alizarda@users.noreply.github.com>
2024-04-22 20:56:49 -0400

    Add files via upload

--- warden.php
+++ warden.php
@@ -84,7 +84,7 @@
                 <section class="panel color3">
                     <div class="span-1">
                             <ul class="contact-icons" style="margin-left: 20px; color: black;">
-                                <li class="fa fa-globe"></li><a href="warden.php">Warden Registry</a><br>
+                                <li class="fa fa-globe"></li><a href="warden.php">Registry</a><br>
                             </ul>					
                         </div>
                 </section>
