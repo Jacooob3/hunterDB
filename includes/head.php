4912845ad0182a7db75d50b751f8ba16d7780202
klattuada <90414276+klattuada@users.noreply.github.com>
2024-04-23 22:04:15 -0400

    Updated head.php with website title

--- includes/head.php
+++ includes/head.php
@@ -1,5 +1,5 @@
 <head>
-		<title>Ethereal by HTML5 UP</title>
+		<title>Hunt.DB</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css" />
