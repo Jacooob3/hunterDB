768b0312751f353c0088e9428120fedbea86ef5f
VermilionAlex <90432346+VermilionAlex@users.noreply.github.com>
2024-04-23 20:31:48 -0400

    Add files via upload

--- logpopevent.php
+++ logpopevent.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
    	require 'includes/database-connection.php';
  
     $warden_id=$_GET['warden_id'];
@@ -27,16 +28,30 @@
                 <div class="span-1">
                     <ul class="contact-icons" style="margin-left: 20px; color: black;">
                         <li class="fa fa-home"></li><a href="index.php">Home</a><br>
-                        <li class="fa fa-globe"></li><a href="about.php">About</a><br>
-                        <li class="fa fa-cogs"></li><a href="lookup.php">Lookup</a><br>
-                        <li class="fa fa-cogs"></li><a href="update.php">Warden</a><br>
-                        <li class="fa fa-globe"></li><a href="account.php">Account</a><br>
-                        <li class="fa fa-globe"></li><a href="login.php">Login</a><br>
-                        <li class="fa fa-globe"></li><a href="signup.php">Sign Up</a><br>
+                        <li class="fa fa-address-card"></li><a href="about.php">About</a><br>
+                        <li class="fa fa-search"></li><a href="lookup.php">Lookup</a><br>
+                        <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']): ?>
+                            <?php if ($_SESSION['role'] == 'warden'): ?>
+                                <li class="fa fa-tag"></li><a href="update.php">Warden</a><br>
+                            <?php endif; ?>
+                            <li class="fa fa-user"></li><a href="account.php">Account</a><br>
+                            <li class="fa fa-user-lock"></li><a href="logout.php">Logout</a><br>
+                        <?php else: ?>
+                            <li class="fa fa-user-lock"></li><a href="login.php">Login</a><br>
+                            <li class="fa fa-user-plus"></li><a href="signup.php">Sign Up</a><br>
+                        <?php endif; ?>
                     </ul>
                 </div>
             </section>
 
+            <section class="panel color3">
+                <div class="span-1">
+                    <ul class="contact-icons" style="margin-left: 20px; color: black;">
+                        <li class="fa fa-user-tag"></li><a href="warden.php">Registry</a><br>
+                        <li class="fa fa-paw"></li><a href="animalpop.php">Animal</a><br>
+                    </ul>
+                </div>
+            </section>
             <!-- Panel -->
             <section class="panel color2-alt">
                 <div class="inner columns aligned">
@@ -46,11 +61,11 @@
 				<div class="hunter-details">
                     <h3><?= $warden_id ?></h3>
 			        <hr />
-                    <h3><?= $animal_id ?></h3>
+                    <h3> Animal ID: <?= $animal_id ?></h3>
 			        <hr />
-                    <h3><?= $state_id ?></h3>
+                    <h3> State: <?= $state_id ?></h3>
 			        <hr />
-                    <h3><?= $population ?></h3>
+                    <h3>Population: <?= $population ?></h3>
 			        <hr />
                 </div>
 			</div>
@@ -58,5 +73,12 @@
                     </div>
                 </div>
             </section>
+
+
+            <!-- Scripts -->
+            <script src="assets/js/jquery.min.js"></script>
+            <script src="assets/js/browser.min.js"></script>
+            <script src="assets/js/breakpoints.min.js"></script>
+            <script src="assets/js/main.js"></script>
     </body>
 </html>
