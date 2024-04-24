34e7a7114ef5efad484b309ec626d72af7b16e82
Jacob Hyun <98486911+Jacooob3@users.noreply.github.com>
2024-04-23 21:04:09 -0400

    test

--- postlogin-hunter.php
+++ postlogin-hunter.php
@@ -1,34 +1,34 @@
 <?php
-session_start();
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-require 'includes/database-connection.php';
+    session_start();
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    require 'includes/database-connection.php';
 
-$email = trim($_POST['email']);
-$pass = trim($_POST['password']);
+    $email = trim($_POST['email']);
+    $pass = trim($_POST['password']);
 
-// Prepare a statement for execution
-$stmt = $pdo->prepare("SELECT hunter_id, pass FROM hunter WHERE email = :email");
-$stmt->bindParam(':email', $email);
-$stmt->execute();
+    // Prepare a statement for execution
+    $stmt = $pdo->prepare("SELECT hunter_id, pass FROM hunter WHERE email = :email");
+    $stmt->bindParam(':email', $email);
+    $stmt->execute();
 
-$user = $stmt->fetch(PDO::FETCH_ASSOC);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-$hashed_password = password_hash($pass, PASSWORD_DEFAULT);
+    $hashed_password = password_hash($pass, PASSWORD_DEFAULT);
 
-if ($user && ($user['pass'] == $hashed_password)) {
-    // Successful login
-    $_SESSION['logged_in'] = true;
-    $_SESSION['role'] = 'hunter';
-    $_SESSION['id'] = $user['hunter_id'];
-    $redirectUrl = 'account.php';
-    $message = "Login successful. Redirecting to account page...";
-} else {
-    // Invalid credentials
-    $_SESSION['error'] = 'Invalid username or password';
-    $redirectUrl = 'login.php';
-    $message = "Login unsuccessful. Redirecting back to login page...";
-}
+    if ($user && ($user['pass'] == $hashed_password)) {
+        // Successful login
+        $_SESSION['logged_in'] = true;
+        $_SESSION['role'] = 'hunter';
+        $_SESSION['id'] = $user['hunter_id'];
+        $redirectUrl = 'account.php';
+        $message = "Login successful. Redirecting to account page...";
+    } else {
+        // Invalid credentials
+        $_SESSION['error'] = 'Invalid username or password';
+        $redirectUrl = 'login.php';
+        $message = $user['pass'] . " " . $hashed_password . " Login unsuccessful. Redirecting back to login page...";
+    }
 ?>
 
 <!DOCTYPE html>
