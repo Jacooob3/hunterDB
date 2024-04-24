fe8d7a27e5586cb71d40a06d339fe62a88a24e50
Jacob Hyun <98486911+Jacooob3@users.noreply.github.com>
2024-04-23 20:22:32 -0400

    fix

--- postlogin.php
+++ postlogin.php
@@ -1,11 +1,11 @@
 <?php
+session_start();
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-session_start();
 require 'includes/database-connection.php';
 
-$email = $_POST['email'];
-$pass = $_POST['password'];
+$email = trim($_POST['email']);
+$pass = trim($_POST['password']);
 
 // Prepare a statement for execution
 $stmt = $pdo->prepare("SELECT warden_id, pass FROM game_warden WHERE email = :email");
