1359f89c22aad6c46516e32289fda652db5b79b5
klattuada <90414276+klattuada@users.noreply.github.com>
2024-04-23 21:46:58 -0400

    fixed display for log kill event

--- logkillevent.php
+++ logkillevent.php
@@ -22,13 +22,69 @@
 <html>
 	<?php include 'includes/head.php';?>
     <body>
-        <?php include 'includes/header.php';?>
+    <!-- Page Wrapper -->
+    <div id="page-wrapper">
+        <!-- Wrapper -->
+        <div id="wrapper">
+
+            <!-- Panel (Sidebar) -->
+            <section class="panel color6">
+                <div class="span-1">
+                    <ul class="contact-icons" style="margin-left: 20px; color: black;">
+                        <li class="fa fa-home"></li><a href="index.php">Home</a><br>
+                        <li class="fa fa-address-card"></li><a href="about.php">About</a><br>
+                        <li class="fa fa-search"></li><a href="lookup.php">Lookup</a><br>
+                        <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']): ?>
+                            <?php if ($_SESSION['role'] == 'warden'): ?>
+                                <li class="fa fa-tag"></li><a href="update.php">Warden</a><br>
+                            <?php endif; ?>
+                            <li class="fa fa-user"></li><a href="account.php">Account</a><br>
+                            <li class="fa fa-user-lock"></li><a href="logout.php">Logout</a><br>
+                        <?php else: ?>
+                            <li class="fa fa-user-lock"></li><a href="login.php">Login</a><br>
+                            <li class="fa fa-user-plus"></li><a href="signup.php">Sign Up</a><br>
+                        <?php endif; ?>
+                    </ul>
+                </div>
+            </section>
+
+            <section class="panel color3">
+                <div class="span-1">
+                    <ul class="contact-icons" style="margin-left: 20px; color: black;">
+                        <li class="fa fa-user-tag"></li><a href="warden.php">Registry</a><br>
+                        <li class="fa fa-paw"></li><a href="animalpop.php">Animal</a><br>
+                    </ul>
+                </div>
+            </section>
+            <!-- Panel -->
+            <section class="panel color2-alt">
+                <div class="inner columns aligned">
+                    <div class="span-6-25">
  		<main>
              <div class="hunter-details-container">
 				<div class="hunter-details">
-                <h3><?= $sql ?></h1>
+                    <h3> Warden ID: <?= $warden_id ?></h3>
+			        <hr />
+                    <h3> Hunter ID: <?= $hunter_id ?></h3>
+                    <hr />
+                    <h3> Animal ID: <?= $animal_id ?></h3>
+			        <hr />
+                    <h3> State ID: <?= $state_id ?></h3>
+			        <hr />
+                    <h3> Kill Event Registered</h3>
+			        <hr />
                 </div>
 			</div>
         </main>
-	</body>
+                    </div>
+                </div>
+            </section>
+
+
+            <!-- Scripts -->
+            <script src="assets/js/jquery.min.js"></script>
+            <script src="assets/js/browser.min.js"></script>
+            <script src="assets/js/breakpoints.min.js"></script>
+            <script src="assets/js/main.js"></script>
+    </body>
 </html>
